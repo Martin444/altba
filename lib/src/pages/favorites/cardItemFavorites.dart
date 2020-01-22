55f0ac601914e44741feca86b5bf26b8be010020
DarkMarksDoe <darkmarksdoe@gmail.com>
2020-01-21 22:17:44 -0600

    Ventana completa, con elementos: Imagen Precio Modelo Botón Lista con valores definidos, funcional hasta el momento.

--- lib/src/pages/favorites/cardItemFavorites.dart
+++ lib/src/pages/favorites/cardItemFavorites.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
 class cardItemFavorites extends StatelessWidget {
+  String precio = "0000";
+  String modelo = "Modelo";
+  String urlProducto = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtmn5upgGeBTKUvb2HsuMIIOVYe6G-xKTYj_IBUqGtq4UkXoixgw&s";
+  cardItemFavorites(this.precio, this.modelo, this.urlProducto);
   @override
   Widget build(BuildContext context) {
-    final card = Container(
+    final fondoItem = Container(
       height: 120,
       width: MediaQuery.of(context).size.width * .95,
       margin: EdgeInsets.only(top: 10.0),
@@ -13,20 +17,52 @@ class cardItemFavorites extends StatelessWidget {
         shape: BoxShape.rectangle,
       ),
     );
-    final photo = Container(
+    final fotoProducto = Container(
       width: 110.0,
       height: 110.0,
-      margin: EdgeInsets.only(left: 10.0,top: 15.0),
+      margin: EdgeInsets.only(left: 10.0, top: 15.0),
       decoration: BoxDecoration(
         shape: BoxShape.rectangle,
         image: DecorationImage(
             fit: BoxFit.cover,
-            image: NetworkImage(
-                "https://firebasestorage.googleapis.com/v0/b/altba-e47fe.appspot.com/o/Categorias%2FAlhajeros%2FAlhajero%201%2FAlhajero1.jpg?alt=media&token=39a48cf5-d86f-43d2-82dd-132b59ff6aa5")),
+            image: NetworkImage(urlProducto)),
       ),
     );
+    final precioProducto = Container(
+        margin: EdgeInsets.only(left: 130.0, top: 20.0),
+        child: Text(
+          "\$ $precio",
+          style: TextStyle(
+            fontSize: 24.0,
+            color: Color.fromRGBO(210, 134, 25, 100),
+          ),
+        ));
+    final nombreProducto = Container(
+        margin: EdgeInsets.only(left: 130.0, top: 55.0),
+        child: Text(
+          "$modelo",
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.black,
+          ),
+        ));
+    final botonAnadir = Container(
+      margin: EdgeInsets.only(left:130.0, top: 80.0),
+      child: OutlineButton.icon(
+          onPressed: () {},
+          shape: StadiumBorder(),
+          icon: Icon(Icons.shopping_cart),
+          label: Text("Añadir al carrito")),
+    );
+
     return Stack(
-      children: <Widget>[card, photo],
+      children: <Widget>[
+        fondoItem,
+        fotoProducto,
+        precioProducto,
+        nombreProducto,
+        botonAnadir
+      ],
     );
   }
 }
--- lib/src/pages/favorites/listFavorites.dart
+++ lib/src/pages/favorites/listFavorites.dart
@@ -10,11 +10,11 @@ class listFavorites extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
         children: <Widget>[
-          cardItemFavorites(),
-          cardItemFavorites(),
-          cardItemFavorites(),
-          cardItemFavorites(),
-          cardItemFavorites()
+          cardItemFavorites("4,300","Alhajero Roma","https://firebasestorage.googleapis.com/v0/b/altba-e47fe.appspot.com/o/Categorias%2FAlhajeros%2FAlhajero%201%2FAlhajero1.jpg?alt=media&token=39a48cf5-d86f-43d2-82dd-132b59ff6aa5"),
+          cardItemFavorites("2,900","Escritorio Desayunador","https://firebasestorage.googleapis.com/v0/b/altba-e47fe.appspot.com/o/Categorias%2FMesas%2FEscritorioDesayunador%2FEscritorio-Desayunador.jpg?alt=media&token=71ca2b01-089d-4214-827f-da57ad4563e2"),
+          cardItemFavorites("2,500", "Closet Estocolmo","https://firebasestorage.googleapis.com/v0/b/altba-e47fe.appspot.com/o/Categorias%2FClosets%2FClosetEstocolmo%2FCloset-Estocolmo.jpg?alt=media&token=e174166a-d533-491d-995a-69b2e64f463d"),
+          cardItemFavorites("4,500", "Comoda Love","https://firebasestorage.googleapis.com/v0/b/altba-e47fe.appspot.com/o/Categorias%2FComodas%2FComodaLove%2FComoda-Love.jpg?alt=media&token=5ba9ef04-6978-45a8-946e-88a9be1f7ec5"),
+          cardItemFavorites("3,900", "Ropero Oslo","https://firebasestorage.googleapis.com/v0/b/altba-e47fe.appspot.com/o/Categorias%2FRoperos%2FRoperoOslo%2FRopero-Oslo.jpg?alt=media&token=73961653-fb6a-4acb-a3ef-0d27725c1e27")
         ],
       ),
     );
